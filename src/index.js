bfe15da04a0d7bbfe55f0af0d1fa36356617b176
knb729 <78786816+knb729@users.noreply.github.com>
2021-11-03 13:17:52 +0900

    aaa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  //テキストボックスの値を取得して、初期化する
   const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div
   alert(inputText);
 };
 
